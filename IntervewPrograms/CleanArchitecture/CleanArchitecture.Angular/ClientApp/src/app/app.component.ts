e4c4e79f6b95c3e29487b88dac802c79ac32d889
deepak1887 <deepak.net1888@gmail.com>
2025-02-11 17:17:07 +0530

    test functions

--- IntervewPrograms/CleanArchitecture/CleanArchitecture.Angular/ClientApp/src/app/app.component.ts
+++ IntervewPrograms/CleanArchitecture/CleanArchitecture.Angular/ClientApp/src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnChanges {
+  ngOnChanges(changes: SimpleChanges): void {
+      throw new Error('Method not implemented.');
+  }
   title = 'app';
 }
